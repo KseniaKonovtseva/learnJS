f9a6889b2adf7bc74590c2dae467e07b2f53b042
Ksenia Konovtseva <ksenia1229@mail.ru>
2020-10-10 01:22:46 +0300

    feat: work with variables

--- script/script.js
+++ script/script.js
@@ -1,10 +1,16 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 30000;
+let income = 'Фриланс';
+let addExpenses = 'Интернет, кофе, курсы';
+let deposit = true;
+let mission = 60000;
+let period = 6;
+let budgetDay = money/30;
 
-alert ('Any text');
-console.log ('Any text');
-console.log('Check');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('Дневной бюджет составляет ' + budgetDay);
