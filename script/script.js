c8f8abd494e7aba83fa86331c7562c1c4678a395
Ksenia Konovtseva <ksenia1229@mail.ru>
2020-10-10 14:42:28 +0300

    feat: work with cycles

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@ let deposit = true;
 let mission = 60000;
 let period = 6;
 let budgetDay = money/30;
+let num = 266219;
 
 console.log(typeof money);
 console.log(typeof income);
@@ -14,3 +15,19 @@ console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(', '));
 console.log('Дневной бюджет составляет ' + budgetDay);
+
+let mult = 1;
+while (num > 0) {
+  let digit = num % 10;
+  mult = mult * digit;
+  num = Math.trunc(num / 10);
+}
+console.log(mult);
+
+let degree = mult ** 3;
+console.log(degree);
+
+while(degree > 99) {
+  degree = Math.trunc(degree / 10);
+}
+console.log(degree);
